bf9e729c3f0a38413da724eb0b9b6d8488aa7725
Theidron <36164886+Theidron@users.noreply.github.com>
2018-03-18 17:12:08 +0000

    main work folder
    contains all files from ikhalifa7 and I.   Logo created - in moHTML>images> logo.png
    more updates to come

---
+++ WebDesignAssignment/html/maxHTML/WebDesignProject/Script/WebProjectV2_JavaScript.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $("#stud_A").click(function(){
+        $("#hidden_A").slideToggle(800);
+    });
+});
+
+$(document).ready(function(){
+	
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+	
+});
