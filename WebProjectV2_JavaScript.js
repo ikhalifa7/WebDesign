0a742193e06986cd407db5234fc3006449bbd9da
Khesnay <maximilien_avit@hotmail.com>
2018-03-19 12:04:25 +0000

    Add files via upload

--- WebProjectV2_JavaScript.js
+++ WebProjectV2_JavaScript.js
@@ -1,3 +1,5 @@
+
+//Display hidden parts
 $(document).ready(function(){
     $("#stud_A").click(function(){
         $("#hidden_A").slideToggle(800);
@@ -5,7 +7,25 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-	
+    $("#stud_B").click(function(){
+        $("#hidden_B").slideToggle(800);
+    });
+});
+
+$(document).ready(function(){
+    $("#stud_C").click(function(){
+        $("#hidden_C").slideToggle(800);
+    });
+});
+
+$(document).ready(function(){
+    $("#stud_D").click(function(){
+        $("#hidden_D").slideToggle(800);
+    });
+});
+
+$(document).ready(function(){
+
 	//Check to see if the window is top if not then display button
 	$(window).scroll(function(){
 		if ($(this).scrollTop() > 100) {
@@ -14,11 +34,30 @@ $(document).ready(function(){
 			$('.scrollToTop').fadeOut();
 		}
 	});
-	
+
 	//Click event to scroll to top
 	$('.scrollToTop').click(function(){
 		$('html, body').animate({scrollTop : 0},800);
 		return false;
 	});
-	
+
+});
+
+//Click theme change
+
+$(".changeColor2").on("click", function() {
+  var T1ColA = $(this).css("--colorOne");
+  var T1ColB = $(this).css("--colorTwo");
+  var T1ColC = $(this).css("--colorThree");
+  var T1ColD = $(this).css("--colorFour");
+  var T1ColE = $(this).css("--colorFive");
+
+  $(".changeColor").removeClass("active");
+    $(this).addClass("active");
+
+  $(document.body).css("--colorOne", T1ColA);
+  $(document.body).css("--colorTwo", T1ColB);
+  $(document.body).css("--colorThree", T1ColC);
+  $(document.body).css("--colorFour", T1ColD);
+  $(document.body).css("--colorFive", T1ColE);
 });
